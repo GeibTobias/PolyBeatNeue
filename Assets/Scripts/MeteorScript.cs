97cdb7a07720df3c593a529310f355c22785ca3b
GeibTobias <geib02@gw.uni-passau.de>
2019-12-16 00:13:34 +0100

    Meteor and Shield

---
+++ Assets/Scripts/MeteorScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorScript : MonoBehaviour
+{
+    public GameObject shield;
+
+    private ShieldScript shieldScript;
+
+    public GameObject gameManager;
+
+    public float probability;
+
+
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public float radius;
+    public float radiusSpeed;
+    public float rotationSpeed; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+        shield = GameObject.Find("Shield");
+        shieldScript = shield.GetComponent<ShieldScript>();
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meteorMove();
+        float rand = Random.value*100;
+        if (rand >= (101 - probability) - 1)
+        {
+             meteorRain();   
+        }
+    }
+
+    void meteorRain()
+    {
+        if (shieldScript.shieldCapacity >= 1)
+        {
+            shieldScript.hitShield();
+        }
+        else
+        {
+            PlayerScript playerScript = gameManager.GetComponentInChildren<PlayerScript>();
+            playerScript.hitPlayer();
+        }
+    }
+
+    void meteorMove()
+    {
+        transform.RotateAround (center.position, axis, rotationSpeed * Time.deltaTime);
+        var desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+    }
+}
---
+++ Assets/Scripts/ShieldScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class ShieldScript : MonoBehaviour
+{
+    public float shieldCapacity;
+
+    public float maxShield;
+
+    public float minShield;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addShield()
+    {
+        shieldCapacity = Mathf.Min(shieldCapacity + (maxShield - minShield)/10, maxShield);
+    }
+
+    public void hitShield()
+    {
+        shieldCapacity = Mathf.Max(shieldCapacity - 10,minShield); 
+    }
+}
